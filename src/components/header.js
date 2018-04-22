fc8f3706afafd4bd86c9ac6b7aeda9529a03ce61
Derik Oliver <derik.oliver@gmail.com>
2018-04-22 10:48:46 -0700

    Initial Commit

---
+++ src/components/header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 15,    
+    height: 60,
+    elevation: 2,
+    position: 'relative'
+  },
+  textStyle: {
+    fontSize: 30
+  }
+};
+
+export default Header;
